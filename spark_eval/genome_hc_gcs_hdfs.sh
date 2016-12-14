dd38b5e63d55ead290a778e98d7f9ecf4336dba6
Tom White <tom@cloudera.com>
2016-12-14 14:24:27 +0000

    Add scripts for Spark eval

---
+++ spark_eval/genome_hc_gcs_hdfs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Run Haplotype Caller on genome data on a GCS Dataproc cluster. Data is in HDFS.
+
+. utils.sh
+
+time_gatk "HaplotypeCallerSpark -I hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit -O hdfs:///user/$USER/q4_spark_eval/out/WGS-G94982-NA12878.vcf -pairHMM AVX_LOGLESS_CACHING" 128 1 4g 4g
