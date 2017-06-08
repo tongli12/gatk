62f052b1c36eb03a1e0696a1c6ce738e4d69164a
Andrey Smirnov <asmirnov2009@gmail.com>
2017-06-08 16:05:27 -0400

    Fixed file paths

--- scripts/cnv_cromwell_tests/germline/run_cnv_germline_workflows.sh
+++ scripts/cnv_cromwell_tests/germline/run_cnv_germline_workflows.sh
@@ -4,24 +4,24 @@ set -e
 script_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$script_path"
 
-ln -fs /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/cnv_common_tasks.wdl
-ln -fs /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl
-ln -fs /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl
-ln -fs /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl
+ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/cnv_common_tasks.wdl
+ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl
+ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl
+ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl
 
 CROMWELL_JAR="cromwell-0.26.jar"
 
 # Panel WES
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl gCNV_panel_creation_workflow_wes.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl gCNV_panel_creation_workflow_wes.json
 # Panel WGS
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl gCNV_panel_creation_workflow_wgs.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_panel_creation_workflow.wdl gCNV_panel_creation_workflow_wgs.json
 
 # Single sample WES calling
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl gCNV_single_sample_calling_workflow_wes.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl gCNV_single_sample_calling_workflow_wes.json
 # Single sample WGS calling
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl gCNV_single_sample_calling_workflow_wgs.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_single_sample_calling_workflow.wdl gCNV_single_sample_calling_workflow_wgs.json
 
 # Cohort WES calling
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl gCNV_cohort_calling_workflow_wes.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl gCNV_cohort_calling_workflow_wes.json
 # Cohort WGS calling
-java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk-protected/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl gCNV_cohort_calling_workflow_wgs.json
+java -jar ~/${CROMWELL_JAR} run /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/germline/gCNV_cohort_calling_workflow.wdl gCNV_cohort_calling_workflow_wgs.json
--- scripts/cnv_cromwell_tests/somatic/run_cnv_somatic_workflows.sh
+++ scripts/cnv_cromwell_tests/somatic/run_cnv_somatic_workflows.sh
@@ -4,7 +4,7 @@ set -e
 script_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$script_path"
 
-ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/cnv_somatic_tasks.wdl
+ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/cnv_common_tasks.wdl
 ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/somatic/cnv_somatic_copy_ratio_bam_workflow.wdl
 ln -fs /home/travis/build/broadinstitute/gatk/scripts/cnv_wdl/somatic/cnv_somatic_allele_fraction_pair_workflow.wdl
 
